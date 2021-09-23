fbf5cc59145fca15c5da4932668647ac4ac40608
liyonglu <liyonglu@100tal.com>
2021-09-23 22:59:46 +0800

    feat: Vue初始化-实例成员
    feat: Vue初始化-实例成员

--- src/core/instance/state.js
+++ src/core/instance/state.js
@@ -350,12 +350,16 @@ export function stateMixin (Vue: Class<Component>) {
     cb: any,
     options?: Object
   ): Function {
+    // 获取Vue实例this
     const vm: Component = this
     if (isPlainObject(cb)) {
+      // 判断如果cb是对象执行 createWatcher
       return createWatcher(vm, expOrFn, cb, options)
     }
     options = options || {}
+    // 标记为用户watcher
     options.user = true
+    // 创建用户watcher对象
     const watcher = new Watcher(vm, expOrFn, cb, options)
     if (options.immediate) {
       const info = `callback for immediate watcher "${watcher.expression}"`
