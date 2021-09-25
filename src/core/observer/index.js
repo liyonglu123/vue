8c59b36e5e18f08de9b8381514c6d66a716a9243
liyonglu <liyonglu@100tal.com>
2021-09-25 20:44:56 +0800

    feat: set源码
    feat: set源码

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -229,16 +229,22 @@ export function set (target: Array<any> | Object, key: any, val: any): any {
   ) {
     warn(`Cannot set reactive property on undefined, null, or primitive value: ${(target: any)}`)
   }
+  // 判断 target 是否是对象， key 是否是合法的索引
   if (Array.isArray(target) && isValidArrayIndex(key)) {
     target.length = Math.max(target.length, key)
+    // 通过 splice 对key位置的元素进行替换
+    // splice 在 array.js 进行了响应化的处理
     target.splice(key, 1, val)
     return val
   }
+  // 如果key 在对象中已经存在直接赋值
   if (key in target && !(key in Object.prototype)) {
     target[key] = val
     return val
   }
+  // 获取 target 中的 observe 对象
   const ob = (target: any).__ob__
+  // 如果target是Vue实例或者$data直接返回
   if (target._isVue || (ob && ob.vmCount)) {
     process.env.NODE_ENV !== 'production' && warn(
       'Avoid adding reactive properties to a Vue instance or its root $data ' +
@@ -246,11 +252,14 @@ export function set (target: Array<any> | Object, key: any, val: any): any {
     )
     return val
   }
+  // 如果 ob 不存在，target不是响应式对象直接赋值
   if (!ob) {
     target[key] = val
     return val
   }
+  // 把 key 设置为响应式属性
   defineReactive(ob.value, key, val)
+  // 发送通知
   ob.dep.notify()
   return val
 }
